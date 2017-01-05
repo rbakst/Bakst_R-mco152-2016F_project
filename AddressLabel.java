b2842c6c7f6f2bf043f02534c59b9f18220b0869
MalkaSchechter <mschecht4@student.touro.edu>
2017-01-04 19:58:07 -0500

    added print Address Labels functionality

--- AddressLabel.java
+++ AddressLabel.java
@@ -1,13 +1,140 @@
 package shul;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Calendar;
+
+import exceptions.MissingDataException;
+
 public class AddressLabel {
-	
-	
-	
-	public void printMemberAddressLabels()
-	{
-		
-		
+	private String formattedName;
+	private String AddressLine1;
+	private String AddressLine2;
+	/*
+	public AddressLabel(Person a, Person b) {
+		StringBuilder info = new StringBuilder();
+
+		info.append(a.getTitle());
+		info.append(" ");
+		info.append(a.getFirstName());
+		info.append(" ");
+		if (!(a.getMidInitial() == null)) {
+			info.append(a.getMidInitial());
+			info.append(" ");
+		}
+		info.append("and");
+		info.append(b.getTitle());
+		info.append(" ");
+		info.append(b.getFirstName());
+		info.append(" ");
+		if (!(b.getMidInitial() == null)) {
+			info.append(b.getMidInitial());
+			info.append(" ");
+		}
+		info.append(b.getLastName());
+
+		this.formattedName = info.toString();
+
+		Membership aMembership = a.getMembershipByID(a.getMembershipID());
+
 	}
 
+	public AddressLabel() {
+
+	}*/
+
+
+	public static void printAddressLabels(Connection connection) throws SQLException, IOException {
+		String fileName = LocalDate.now().toString() + "AddLabels.txt";
+		PrintWriter writer = new PrintWriter(new FileWriter(fileName, true));
+	    
+		String qry = "select membershipid from membership";
+
+
+		PreparedStatement statement = connection.prepareStatement(qry, ResultSet.TYPE_SCROLL_SENSITIVE,
+				ResultSet.CONCUR_UPDATABLE);
+
+		ResultSet rsMem = statement.executeQuery();;
+		String aptNum = "",st ="",city="",state="", zip="";
+
+		AddressLabel a = new AddressLabel();
+		
+		while (rsMem.next()) {
+			String qry2 = "select * from person p join Membership m on p.MembershipID = m.MembershipID join address a on a.addressid = m.MembershipAddress where m.membershipid = ?";
+
+			PreparedStatement statement2 = connection.prepareStatement(qry2, ResultSet.TYPE_SCROLL_SENSITIVE,
+					ResultSet.CONCUR_UPDATABLE);
+
+
+			
+			statement2.setString(1, rsMem.getString("MembershipID"));
+			ResultSet ppl = statement2.executeQuery();
+
+			ppl.last();
+			// 2 rows
+			if (ppl.getRow() == 2) {
+				ppl.first();
+				String title1 = ppl.getString("Title");
+				String fname1 = ppl.getString("fname");
+				String lname = ppl.getString("lname");
+				
+				ppl.next();
+				
+				String title2 = ppl.getString("Title");
+				String fname2 = ppl.getString("fname");
+				
+				
+				a.formattedName = title1 + " and " + title2 + " " + fname1 + " and " + fname2 + " " + lname;
+			
+			} else {
+				ppl.first();
+				String title = ppl.getString("Title");
+				String fname = ppl.getString("fname");
+				String lname = ppl.getString("lname");
+				
+				a.formattedName = title + " " + fname + " " + lname;
+				
+			}
+
+			st = ppl.getString("street");
+			aptNum = ppl.getString("aptNum");
+			city = ppl.getString("city");
+			state = ppl.getString("addrstate");
+			zip = ppl.getString("zip");
+
+	if (aptNum.equals("null")) {
+		a.AddressLine1 = st;
+		
+	}else{
+		a.AddressLine1 =st +" Apt" + aptNum;
+	}
+	a.AddressLine2 = city + ", " + state + " " + zip;
+	
+	
+	writer.println(a.formattedName);
+	writer.println(a.AddressLine1);
+	writer.println(a.AddressLine2);
+	writer.println("");
+		}
+		
+		
+		writer.close();
+		
+		File f = new File(fileName);
+		Desktop d = Desktop.getDesktop();
+		d.open(f);
 }
+}
+
+
+
--- UseShul.java
+++ UseShul.java
@@ -1,6 +1,13 @@
 package shul;
+
 import exceptions.*;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -8,77 +15,266 @@ import java.util.Scanner;
 
 public class UseShul {
 
-	public static void main(String [] args) 
-	{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
-		ArrayList<Person> personsToBeKeptInShulClass = new ArrayList<>();
+		// ArrayList<Person> personsToBeKeptInShulClass = new ArrayList<>();
 		int choice = 0;
-		while (choice != 3)
-		{
-			choice = menu();
-			
-			switch(choice)
-			{
-			case 1:
-				try{
-					addPerson(personsToBeKeptInShulClass);
-				}catch(MissingDataException ex)
-				{
-					System.out.println(ex.getMessage());
-				}
-				break;
-			case 2:
-				System.out.println(personsToBeKeptInShulClass);
-				break;
-			case 3:
-				System.exit(0);
-				default:
+
+		String connectionString = "jdbc:sqlserver://MININT-9UUBA0C\\SQLEXPRESS;username=ShulProject;password=ShulDatabase";
+
+		try {
+			Connection connection = DriverManager.getConnection(connectionString);
+			connection.setAutoCommit(true);
+
+			while (choice != 5) {
+				choice = menu();
+
+				switch (choice) {
+				case 1:
+					System.out.println("Adding a new Membership:");
+					System.out.println("Is the membership address already in the system? (Y/N)");
+					char answer = input.next().charAt(0);
+
+					while (answer != 'Y' && answer != 'y' && answer != 'N' && answer != 'n') {
+						System.out.println("Invalid Input.  Please answer Y/N.");
+						answer = input.next().charAt(0);
+					}
+
+					if (answer == 'Y' || answer == 'y') {
+						System.out.println("Do you have the Address ID? (Y/N)");
+						answer = input.next().charAt(0);
+
+						while (answer != 'Y' && answer != 'y' && answer != 'N' && answer != 'n') {
+							System.out.println("Invalid Input.  Please answer Y/N.");
+							answer = input.next().charAt(0);
+						}
+						if (answer == 'N' || answer == 'n') {
+
+							// really this code should allow user to search for
+							// address by some criteria
+							// now it does nothing.
+
+							// System.out
+							// .println("Search for the Address by Last Name and
+							// keep record of the Address ID: ");
+
+							// ArrayList<Address> addresses =
+							// searchForAddByLName(connection);
+
+							try {
+								Membership aMembership = addMembershipWithAddID(connection);
+
+								System.out.println("Membership successfully added: ");
+								System.out.println(aMembership.toString());
+							} catch (MissingDataException e) {
+								System.out.println(e.getMessage());
+							} catch (Exception e) {
+								System.out.println("Invalid Address State");
+							}
+						} else {
+							try {
+								Membership aMembership = addMembershipWithAddID(connection);
+
+								System.out.println("Membership successfully added: ");
+								System.out.println(aMembership.toString());
+							} catch (MissingDataException e) {
+								System.out.println(e.getMessage());
+							}
+						}
+
+					} else {
+						try {
+							Membership aMembership = addMembershipWithNewAddress(connection);
+
+							System.out.println("Membership successfully added: ");
+							System.out.println(aMembership.toString());
+						} catch (MissingDataException e) {
+							System.out.println(e.getMessage());
+						}
+
+					}
+					break;
+				case 2:
+					try {
+						Person aPerson = addPersonToExistingMembership(connection);
+
+						System.out.println("\n Person successfully added:");
+						System.out.println(aPerson.toString());
+
+					} catch (MissingDataException ex) {
+						System.out.println(ex.getMessage());
+					}
+					break;
+				case 3:
+					Address add;
+					try {
+						add = addAddress(connection);
+						System.out.println("New Address Successfully Added:\n" + add.toString());
+					} catch (MissingDataException e) {
+						System.out.println(e.getMessage());
+					}
+
+					break;
+				case 4:
+					ArrayList<Person> persons;
+					try {
+						persons = Person.getAllPeople(connection);
 					
+
+					if (!persons.isEmpty()) {
+
+						StringBuilder info = new StringBuilder();
+						info.append("People entered into the system: ");
+						for (Person p : persons) {
+							info.append(p.toString());
+						}
+
+						System.out.println(info.toString());
+					} else
+						System.out.println("There are no people entered in the system.\n");
+					} catch (MissingDataException e) {
+						System.out.println(e.getMessage());
+					}
+
+					break;
+				case 5:
+					try {
+						AddressLabel.printAddressLabels(connection);
+					} catch (FileNotFoundException | UnsupportedEncodingException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					
+					break;
+				case 6:
+					connection.close();
+					System.exit(0);
+				default:
+
+				}
 			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
 		}
-		
+
 	}
-	
-	public static int menu()
-	{
-		System.out.println("1. Add a Person");
-		System.out.println("2. Display Persons");
-		System.out.println("3. Exit");
-		
+
+	public static int menu() {
+		System.out.println("1. Add a Membership");
+		System.out.println("2. Add a Person to an existing membership");
+		System.out.println("3. Add an Address");
+		System.out.println("4. Display Persons");
+		System.out.println("5. Print Address Labels for all People");
+		System.out.println("6. Exit");
+
 		Scanner keyboard = new Scanner(System.in);
 		int choice = keyboard.nextInt();
-		
+
 		return choice;
 	}
-	
-	public static void addPerson(ArrayList<Person> personsToBeKeptInShulClass) throws MissingDataException
-	{
-		Scanner keyboard = new Scanner (System.in);
+
+	public static Person addPersonToExistingMembership(Connection connection) throws SQLException, MissingDataException{
+		Scanner keyboard = new Scanner(System.in);
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-		
+		String midInitial = null;
+		LocalDate dob = null;
+
 		System.out.println("Title:");
 		String title = keyboard.next();
 		System.out.println("First Name: ");
 		String firstName = keyboard.next();
 		System.out.println("Last Name: ");
 		String lastName = keyboard.next();
-		System.out.println("Middle Initial: ");
-		char midInitial = keyboard.next().charAt(0);
-		System.out.println("Date of Birth (mm/dd/yyyy): ");
-		LocalDate DOB = LocalDate.parse(keyboard.next(), formatter);
+		System.out.println("Is there a middle initial?");
+		char answer = keyboard.next().charAt(0);
+		if (answer == 'y' || answer == 'Y') {
+			System.out.println("Middle Initial: ");
+			midInitial = keyboard.next().substring(0, 0);
+		}
+		System.out.println("Do you have the DOB?");
+		answer = keyboard.next().charAt(0);
+		if (answer == 'y' || answer == 'Y') {
+			System.out.println("Date of Birth (mm/dd/yyyy): ");
+			dob = LocalDate.parse(keyboard.next(), formatter);
+		}
 		keyboard.nextLine();
 		System.out.println("Gender: (m/f)");
-		char gender = keyboard.next().charAt(0);
+		String gender = keyboard.next();
 		System.out.println("EmailAddress: ");
 		String emailAddress = keyboard.next();
 		System.out.println("Membership ID: ");
 		String membershipID = keyboard.next();
-		
-		Person aPerson = new Person(title,firstName,midInitial,lastName,DOB, gender,emailAddress, membershipID);
-		personsToBeKeptInShulClass.add(aPerson);
-		
-	
-	}
-	
 
+		
+		Membership mem = Membership.getMembershipByID(membershipID, connection);
+		Person aPerson = Person.addPersonToDB(mem, title, firstName, midInitial, lastName, dob, emailAddress,
+				gender, connection);
+
+		return aPerson;
+	}
+
+	public static Address addAddress(Connection connection) throws MissingDataException, SQLException {
+		Scanner keyboard = new Scanner(System.in);
+		String aptNum = null;
+		System.out.println("Add a New Address:");
+		System.out.println("Street Address:");
+		String street = keyboard.nextLine();
+		System.out.println("Is there an Apt Number?");
+		char answer = keyboard.next().charAt(0);
+		if (answer == 'y' || answer == 'Y') {
+			System.out.println("Apt Number: ");
+			aptNum = keyboard.next();
+		}
+		System.out.println("City: ");
+		keyboard.nextLine();
+		String city = keyboard.nextLine();
+		System.out.println("State: ");
+		String state = keyboard.nextLine();
+		System.out.println("Zip Code: ");
+		String zip = keyboard.nextLine();
+
+		Address add = Address.addAddressToDB(street, aptNum, city, state, zip, connection);
+
+		return add;
+	}
+
+	public static Membership addMembershipWithAddID(Connection connection) throws MissingDataException, SQLException{
+		Scanner keyboard = new Scanner(System.in);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
+		System.out.println("Membership Type:  (FULL, ASSOCIATE, of FRIEND)");
+		MembershipType memType = MembershipType.valueOf(keyboard.next().toUpperCase());
+		System.out.println("AddressID:");
+		int addID = keyboard.nextInt();
+
+		Membership aMembership = Membership.addMembershipToDB(memType, Address.getAddressByID(addID, connection), connection);
+
+		return aMembership;
+	}
+
+	public static Membership addMembershipWithNewAddress(Connection connection) throws MissingDataException, SQLException {
+		Scanner keyboard = new Scanner(System.in);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
+		System.out.println("Membership Type:  (FULL, ASSOCIATE, or FRIEND)");
+		MembershipType memType = MembershipType.valueOf(keyboard.next().toUpperCase());
+
+		Address add = addAddress(connection);
+
+		Membership aMembership = Membership.addMembershipToDB(memType, add, connection);
+
+		return aMembership;
+
+	}
+	/*
+	 * public ArrayList<Address> searchForAddByLName(Connection connection) {
+	 * Scanner keyboard = new Scanner(System.in); System.out.println(
+	 * "Please Enter the street Address: "); String lName = keyboard.next();
+	 * 
+	 * Person p =
+	 * 
+	 * Membership aMembership = getMembershipByLName(lName, connection); }
+	 */
 }
